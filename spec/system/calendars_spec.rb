39074b03c84096fe391500832d32afad5e23d75f
Fukumura Shunsuke <shunshun2967@gmail.com>
2021-12-10 03:37:01 -0800

    カレンダータイトル表示が正しく動作するかのテストを追加

--- spec/system/calendars_spec.rb
+++ spec/system/calendars_spec.rb
@@ -4,7 +4,9 @@ describe 'カレンダー管理機能', type: :system, js: true do
   describe 'カレンダー表示機能' do
     before do
       user_a = FactoryBot.create(:user, name: 'ユーザーA',email: 'a@email.com',password: 'password',benchpress: 10,squat: 50,deadlift: 100 )
-      FactoryBot.create(:calendar, title: 'テストをします', memo: 'テスト用メモ' )
+      FactoryBot.create(:user, name: 'ユーザーB',email: 'b@email.com',password: 'password',benchpress: 100,squat: 500,deadlift: 1000 )
+      FactoryBot.create(:calendar, title: 'テストをします', start_time: Time.now ,memo: 'テスト用メモ', user: user_a )
+      FactoryBot.create(:calendar, title: 'テストをします2', start_time: Time.now.yesterday ,memo: 'テスト用メモ2', user: user_a )
     end
 
     context 'ユーザーAがログインしているとき' do
@@ -15,7 +17,7 @@ describe 'カレンダー管理機能', type: :system, js: true do
         click_button 'session_button'
       end
 
-      it 'ログインしたときにログインしましたと表示される' do
+      it 'ユーザーAがログインしたときにログインしましたと表示される' do
         expect(page).to have_content 'ログインしました。'
       end
 
@@ -29,6 +31,33 @@ describe 'カレンダー管理機能', type: :system, js: true do
         within '.deadlift_max' do
           expect(page).to have_content '100'
         end
+      end
+
+      it 'ユーザーAが今日の日付で作成したタイトルが今日のカレンダーの場所に表示される' do
+        within '.today' do
+          expect(page).to have_content 'テストをします'
+        end
+      end
+
+      it 'ユーザーAが前日の日付で作成したタイトルが前日のカレンダーの場所に表示される' do
+        within '.past.current-month.has-events' do
+          expect(page).to have_content 'テストをします2'
+        end
+      end
+
+      it 'ユーザーAが前日の日付で作成したタイトルが今日のカレンダーの場所には表示されない' do
+        within '.today' do
+          expect(page).to have_content 'テストをします'
+        end
+      end
+    end
+
+    context "ユーザーBがログインしているとき" do
+      before do
+        visit sessions_new_path
+        fill_in "session_email", with: 'b@email.com'
+        fill_in "session_password", with: 'password'
+        click_button 'session_button'
       end
     end
   end
