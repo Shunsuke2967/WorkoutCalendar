bb66ba51b92f68e72262b02669eaf73686c26236
Fukumura Shunsuke <shunshun2967@gmail.com>
2021-12-10 22:49:37 -0800

    メモを自分の作成したものしか見れないように変更しました

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,22 +15,22 @@ class CalendarsController < ApplicationController
   end
 
   def show
-    @calendar = current_user..find(params[:id])
+    @calendar = current_user.calendar.find(params[:id])
   end
 
   def edit
-    @calendar = current_user..find(params[:id])
+    @calendar = current_user.calendar.find(params[:id])
   end
 
   def update
-    calendar = current_user..find(params[:id])
+    calendar =current_user.calendar.find(params[:id])
     if calendar.update(calendar_params)
       redirect_to calendar_path, notice: "#{calendar.start_time.strftime("%Y年%m月%d日")}のメモを変更しました"
     end
   end
 
   def destroy
-    calendar = current_user..find(params[:id])
+    calendar =current_user.calendar.find(params[:id])
     calendar.destroy
     redirect_to root_path, notice: "#{calendar.start_time.strftime("%Y年%m月%d日")}の#{calendar.title}を削除しました"
   end
