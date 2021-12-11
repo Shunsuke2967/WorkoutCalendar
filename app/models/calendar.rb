ec9a37776310c81aa79002ec39371d226ae999d9
Fukumura Shunsuke <shunshun2967@gmail.com>
2021-12-10 21:24:17 -0800

    ボタンの配置調整

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,4 +1,6 @@
 class Calendar < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
+  validates :title, length: { maximum: 10 }
+  validates :memo, length: { maximum: 150 }
 end
