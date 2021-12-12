c3d26eb81fb81593ed53531728c75a158eaa1930
Fukumura Shunsuke <shunshun2967@gmail.com>
2021-12-11 20:33:11 -0800

    heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,15 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.google[:gmail_name],
+    password: Rails.application.credentials.google[:gmail_password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
